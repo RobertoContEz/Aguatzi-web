6d516548444ab5ac040243c2c765798745f8c424
RobertoContEz <95501242+RobertoContEz@users.noreply.github.com>
2023-11-14 04:02:54 -0700

    modelo y servelts

---
+++ src/java/modelo/Producto.java
@@ -0,0 +1,76 @@
+
+package modelo;
+
+/**
+ *
+ * @author rc0nt
+ */
+public class Producto {
+    private int id;
+    private String nombre;
+    private String img;
+    private double precio;
+    private String tipo;
+    private int stock;
+
+    public Producto() {
+    }
+
+    public Producto(int id, String nombre, String img, double precio, String tipo, int stock) {
+        this.id = id;
+        this.nombre = nombre;
+        this.img = img;
+        this.precio = precio;
+        this.tipo = tipo;
+        this.stock = stock;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    
+}
